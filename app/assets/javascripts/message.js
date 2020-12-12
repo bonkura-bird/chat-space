19a52415b78b23d0afe75710aadbf5da831bc94d
bonkura-bird <dia8856@gmail.com>
2020-12-12 12:12:33 +0900

    create html from json

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,34 @@
 $(function(){
+  function buildHTML(message){
+    if (message.image) {
+      let html = `<div class="MessageList__messageBox">
+                    <div class="MessageList__name">
+                    ${message.user_name}
+                      <div class="MessageList__date">
+                      ${message.created_at}
+                      </div>
+                    </div>
+                    <p class="MessageList__message">
+                    ${message.body}
+                    </p>
+                    <img class="Message__image" src="${message.image}">
+                  </div>`
+       return html;
+    } else {
+      let html = `<div class="MessageList__messageBox">
+                    <div class="MessageList__name">
+                    ${message.user_name}
+                      <div class="MessageList__date">
+                      ${message.created_at}
+                      </div>
+                    </div>
+                    <p class="MessageList__message">
+                    ${message.body}
+                    </p>
+                  </div>`
+      return html;
+    };
+  }
   $(".Form").on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -11,5 +41,8 @@ $(function(){
       processData: false,
       contentType: false
     })
-  })
+    .done(function(data){
+      let html = buildHTML(data);
+    })
+  });
 });
