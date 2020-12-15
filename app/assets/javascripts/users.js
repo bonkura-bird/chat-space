d0b4b807d6ef4c39410f64b7b0233bb010447b22
bonkura-bird <dia8856@gmail.com>
2020-12-15 14:47:15 +0900

    incremental search, ~remove DOM

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,17 +1,49 @@
 $(function(){
-  $("#UserSearch__field").on("keyup", function(){
+  function addUser(user){
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">${user.name}</p>
+                  <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+                `;
+    $("#UserSearchResult").append(html);
+  }
+
+  function  addNoUser(){
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーが見つかりません</p>
+                </div>
+                `;
+    $("#UserSearchResult").append(html);
+  }
+
+  $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
     $.ajax({
-      type: "GET",    //HTTPメソッド
-      url: "/users",       //users_controllerの、indexアクションにリクエストの送信先を設定する
-      dataType: 'json',
-      data: input
+      type: "GET",
+      url: "/users",
+      data: { keyword: input },
+      dataType: 'json'
     })
     .done(function(users) {
-      console.log("clear");
+      $("#UserSearchResult").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      }else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
     .fail(function() {
-      console.log("not clear");
+      alert("通信エラーです。ユーザーが表示できません。");
     });
   })
+
+  $("#UserSearchResult").on("click", ".ChatMember", function(){
+    $(this).remove(".ChatMember");
+  });
 });
